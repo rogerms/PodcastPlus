ad9ed6860bd2242a99835e0891e56367f8cd917a
Roger Santos <sirsroger@gmail.com>
2013-11-15 10:23:24 -0700

    restart

---
+++ PodcastPlus/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace PodcastPlus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            PodcastClient pc = new PodcastClient();
+            List<PodcastFeed> feed = pc.RetrieveFeedAsync(@"C:\Users\Powerspeak\SkyDrive\Programming\Podcast.xml");
+
+            Debug.WriteLine("Title :" + feed.Count);
+        }
+    }
+}
